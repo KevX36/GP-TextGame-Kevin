b7ff109f17c87b0ed16ec20b0e398ad118f96121
KevX36 <kevincard2007@gmail.com>
2026-03-30 08:40:56 -0300

    Logic for game moved to game manager

--- GP-TextGame-Kevin/GameManager.cs
+++ GP-TextGame-Kevin/GameManager.cs
@@ -27,7 +27,100 @@ namespace GP_TextGame_Kevin
             
             
         }
-        
+        public static void RemoveUsedOrDead()
+        {
+            List<int> enemiesToRemove = new List<int>();
+            List<int> itemsToRemove = new List<int>();
+            //removes dead enemies
+            if (EnemyManager.enemies.Any())
+            {
+                for (int i = 0; i < EnemyManager.enemies.Count; i++)
+                {
+                    if (!EnemyManager.enemies[i]._health.CheakIfAlive())
+                    {
+                        GameManager.map.redrawSpace(EnemyManager.enemies[i]._xPos, EnemyManager.enemies[i]._yPos);
+                        enemiesToRemove.Add(i);
+                    }
+                }
+                if (enemiesToRemove.Any())
+                {
+                    for (int i = 0; i < enemiesToRemove.Count; i++)
+                    {
+
+                        GameManager.player._lastEnemy = 0;
+                        EnemyManager.enemies.Remove(EnemyManager.enemies[enemiesToRemove[i]]);
+                        GameManager.Score += 10;
+                    }
+                    enemiesToRemove.Clear();
+                }
+            }
+
+            //removes used items
+            if (Item_manager.items.Any())
+            {
+                for (int i = 0; i < Item_manager.items.Count; i++)
+                {
+                    if (Item_manager.items[i].hasBeenUsed)
+                    {
+                        GameManager.map.redrawSpace(Item_manager.items[i]._xPos, Item_manager.items[i]._yPos);
+                        itemsToRemove.Add(i);
+                    }
+                }
+                if (itemsToRemove.Any())
+                {
+                    for (int i = 0; i < itemsToRemove.Count; i++)
+                    {
+
+
+
+
+                        Item_manager.items.Remove(Item_manager.items[itemsToRemove[i]]);
+
+                    }
+                    itemsToRemove.Clear();
+                }
+            }
+        }
+        public static void draw()
+        {
+            GameManager.player.DrawPlayer();
+            EnemyManager.boss.DrawEnemy();
+            if (Item_manager.items.Any())
+            {
+                for (int i = 0; i < Item_manager.items.Count; i++)
+                {
+
+                    Item_manager.items[i].Draw();
+
+                }
+            }
+
+            if (EnemyManager.enemies.Any())
+            {
+                for (int i = 0; i < EnemyManager.enemies.Count; i++)
+                {
+
+                    EnemyManager.enemies[i].DrawEnemy();
+                }
+            }
+        }
+        public static void Move()
+        {
+            GameManager.player.MoveInput();
+            if (EnemyManager.enemies.Any())
+            {
+                for (int i = 0; i < EnemyManager.enemies.Count; i++)
+                {
+
+
+                    EnemyManager.enemies[i].MoveDirection();
+
+                }
+            }
+
+
+            EnemyManager.boss.MoveDirection();
+        }
         public static int forestHeal = 3;
         public static int swampDamage = 3;
         public static int Score = 0;
--- GP-TextGame-Kevin/Program.cs
+++ GP-TextGame-Kevin/Program.cs
@@ -33,103 +33,15 @@ namespace FirstPlayable_GP2_Kevin
                 {
                     Console.SetCursorPosition(0, 0);
                     Console.WriteLine($"Health: {GameManager.player._health._health}    strength:{GameManager.player.strength}     Score: {GameManager.Score}       ExtraLives: {GameManager.player._health._revives}      Enemy Health: {GameManager.player._lastEnemyHP}      Enemy Strength: {GameManager.player._lastEnemyStrength}                 ");
-                    //removes dead enemies
-                    if (EnemyManager.enemies.Any())
-                    {
-                        for (int i = 0; i < EnemyManager.enemies.Count; i++)
-                        {
-                            if (!EnemyManager.enemies[i]._health.CheakIfAlive())
-                            {
-                                GameManager.map.redrawSpace(EnemyManager.enemies[i]._xPos, EnemyManager.enemies[i]._yPos);
-                                enemiesToRemove.Add(i);
-                            }
-                        }
-                        if (enemiesToRemove.Any())
-                        {
-                            for (int i = 0; i < enemiesToRemove.Count; i++)
-                            {
-
-                                GameManager.player._lastEnemy = 0;
-                                EnemyManager.enemies.Remove(EnemyManager.enemies[enemiesToRemove[i]]);
-                                GameManager.Score += 10;
-                            }
-                            enemiesToRemove.Clear();
-                        }
-                    }
-
-                    //removes used items
-                    if (Item_manager.items.Any())
-                    {
-                        for (int i = 0; i < Item_manager.items.Count; i++)
-                        {
-                            if (Item_manager.items[i].hasBeenUsed)
-                            {
-                                GameManager.map.redrawSpace(Item_manager.items[i]._xPos, Item_manager.items[i]._yPos);
-                                itemsToRemove.Add(i);
-                            }
-                        }
-                        if (itemsToRemove.Any())
-                        {
-                            for (int i = 0; i < itemsToRemove.Count; i++)
-                            {
+                    GameManager.RemoveUsedOrDead();
 
 
 
-
-                                Item_manager.items.Remove(Item_manager.items[itemsToRemove[i]]);
-
-                            }
-                            itemsToRemove.Clear();
-                        }
-                    }
+                    GameManager.draw();
 
 
 
-                    GameManager.player.DrawPlayer();
-                    EnemyManager.boss.DrawEnemy();
-                    if (Item_manager.items.Any())
-                    {
-                        for (int i = 0; i < Item_manager.items.Count; i++)
-                        {
-
-                            Item_manager.items[i].Draw();
-
-                        }
-                    }
-
-                    if (EnemyManager.enemies.Any())
-                    {
-                        for (int i = 0; i < EnemyManager.enemies.Count; i++)
-                        {
-
-                            EnemyManager.enemies[i].DrawEnemy();
-                        }
-                    }
-
-
-
-                    GameManager.player.MoveInput();
-                    if (EnemyManager.enemies.Any())
-                    {
-                        for (int i = 0; i < EnemyManager.enemies.Count; i++)
-                        {
-
-
-                            EnemyManager.enemies[i].MoveDirection();
-
-                        }
-                    }
-
-
-                    EnemyManager.boss.MoveDirection();
-
-
-
-
-
-
-
-
+                    GameManager.Move();
 
                 }
                 //end game
